b706a46afa4cb4cff09f9562f1fb9fac987797e3
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-05-19 08:50:55 +0530

    setUp

---
+++ contracts/NFTmarketplace.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+//BELOW 3 LINE OF CODE HELP TO IMPORT NFT CONTRACTS FROM OPENZEPPLIN
+import "@openzeppelin/contracts/utils/Counters.sol"; //this will keep track on nft's
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "hardhat/console.sol";
+
+
+
