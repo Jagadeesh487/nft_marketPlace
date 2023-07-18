fb639697699c92af56783aecf5aa77125651de13
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-07-18 12:53:00 +0530

    completed navbar css

--- frontEnds/my-app/components/NavBar/NavBar.jsx
+++ frontEnds/my-app/components/NavBar/NavBar.jsx
@@ -92,14 +92,14 @@ const NavBar = () => {
           <div className={Style.navbar_container_left_box_input}>
             <div className={Style.navbar_container_left_box_input_box}>
               <input type='text' placeholder='Search NFT'/>
-              <BsSearch onClick={()=> {}} className = {Style.search_con}/>
+              <BsSearch onClick={()=> {}} className = {Style.search_icon}/>
             </div>
           </div>
         </div>
         {/* //end of left section
 
         //starting of right section */}
-        <div className={Style.navbar_container_Right}>
+        <div className={Style.navbar_container_right}>
           <div className={Style.navbar_container_right_discover}>
 
             {/* discover menu */}
