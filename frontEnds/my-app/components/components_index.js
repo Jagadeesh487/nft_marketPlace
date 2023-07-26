79c835673585b70903e1351d696ea3d4d235b0d6
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-07-26 17:45:09 +0530

    completed big nft slider with one error

--- frontEnds/my-app/components/components_index.js
+++ frontEnds/my-app/components/components_index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Fotter";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
 
-export{Button, Footer, NavBar, HeroSection,Service};
+export{Button, Footer, NavBar, HeroSection,Service,BigNFTSlider};
