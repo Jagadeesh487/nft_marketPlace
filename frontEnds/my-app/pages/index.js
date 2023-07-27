3bacff1f4cd5820b09f4d492b9481ffb1e58ea39
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-07-27 08:52:31 +0530

    completed jsx for subscribe component

--- frontEnds/my-app/pages/index.js
+++ frontEnds/my-app/pages/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 // internal import
 import Style from '../styles/index.module.css';
-import { HeroSection, Service, BigNFTSlider } from "../components/components_index"
+import { HeroSection, Service, BigNFTSlider, Subscribe} from "../components/components_index"
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection/>
       <Service/>
+      <Subscribe/>
       <BigNFTSlider/>
     </div>
   )
