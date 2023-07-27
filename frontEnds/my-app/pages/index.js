635b564b9145998018b9fdf30bec7c537612cba9
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-07-27 16:52:52 +0530

    completed fiter jsx and css part

--- frontEnds/my-app/pages/index.js
+++ frontEnds/my-app/pages/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 // internal import
 import Style from '../styles/index.module.css';
-import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category} from "../components/components_index"
+import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category, Filter} from "../components/components_index"
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection/>
       <Service/>
+      <BigNFTSlider/>
+      <Title 
+      heading="Featured NFT's"
+      paragraph="Discover the most outstanding NFTs in all topics of life"/>
+      <Filter/>
       <Title 
       heading="Browse by category"
       paragraph="Explore the NFTs in the most features categaries"/>
       <Category/>
       <Subscribe/>
-      <BigNFTSlider/>
     </div>
   )
 }
