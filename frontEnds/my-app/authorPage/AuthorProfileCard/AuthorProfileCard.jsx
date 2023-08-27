82e935d7515bc4cfc3f45aa2407e697eb171f944
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-08-27 10:29:01 +0530

    completed autorprofile jsx and css

--- frontEnds/my-app/authorPage/AuthorProfileCard/AuthorProfileCard.jsx
+++ frontEnds/my-app/authorPage/AuthorProfileCard/AuthorProfileCard.jsx
@@ -1,8 +1,8 @@
 import React,{useState} from 'react'
 import Image from 'next/image';
-import {MdVerfied, MdCloudUpload, MdOutLineReportProblem} from 'react-icons/md'
+import {MdVerfied, MdCloudUpload, MdOutLineReportProblem, MdOutlineReportProblem} from 'react-icons/md'
 import {FiCopy} from 'react-icons/fi';
-import { TiSocialFacebook, TiSocialLinkedin, TiSocialYoutube, TiSocialTwitter } from 'react-icons/ti';
+import { TiSocialFacebook, TiSocialLinkedin, TiSocialYoutube, TiSocialTwitter, TiSocialInstagram } from 'react-icons/ti';
 import { BsThreeDots } from 'react-icons/bs';
 import Style from './AuthorProfileCard.module.css'
 import images from '../../img'
@@ -12,13 +12,40 @@ import { MdVerified } from 'react-icons/md';
 const AuthorProfileCard = () => {
     const [share, setShare] = useState(false);
     const [report, setReport] = useState(false);
+
+    const copyAddress = () => {
+        const copyText = document.getElementById("myInput")
+        copyText.select();
+        navigator.clipboard.writeText(copyText.value);
+    }
+
+    const openShare = () => {
+        if(!share){
+            setShare(true);
+            setReport(false);
+        }
+        else{
+            setShare(false)
+        }
+    }
+
+    const openReport = () => {
+        if(!report){
+            setReport(true);
+            setShare(false);
+        }
+        else{
+            setReport(false)
+        }
+    }
+
   return (
     <div className={Style.AuthorProfileCard}>
         <div className={Style.AuthorProfileCard_box}>
             <div className={Style.AuthorProfileCard_box_img}>
                 <Image
                 src = {images.nft_image_1}
-                className={Style.AuthorProfileCard_box_img_imag}
+                className={Style.AuthorProfileCard_box_img_img}
                 alt="NFT IMAGES"
                 width={220}
                 height={220}
@@ -33,13 +60,43 @@ const AuthorProfileCard = () => {
 
                 <div className = {Style.AuthorProfileCard_box_info_address}>
                     <input type="text" value="0x236586582359..E3J667HJ99" id="myInput"/>
-                </div>
-                <FiCopy onClick = {() => copyAddress()} className={Style.AuthorProfileCard_box_info_address_icon}/>
-            </div>
-            <p>
+                    <FiCopy onClick = {() => copyAddress()} className={Style.AuthorProfileCard_box_info_address_icon}/>
+                </div> 
+                <p>
                 Punk #4782 / An OG CryptoPunk collector, hoarder of NFT's.
                 Contributing to @ether_cards. an NFT Monetization Platform.
-            </p>
+                </p>
+                <div className={Style.AuthorProfileCard_box_info_social}>
+                <a href="#"><TiSocialFacebook/></a>
+                <a href="#"><TiSocialInstagram/></a>
+                <a href="#"><TiSocialYoutube/></a>
+                <a href="#"><TiSocialLinkedin/></a>
+                </div>
+            </div>        
+            <div className={Style.AuthorProfileCard_box_share}>
+                <Button btnName="Follow" handelClick={()=>{}}/>
+                <MdCloudUpload onClick = {() => openShare()} className = {Style.AuthorProfileCard_box_share_icon}/>  
+                {share && (
+                    <div className={Style.AuthorProfileCard_box_share_upload}>
+                        <p><span><TiSocialFacebook/></span>{""}Facebook</p>
+                        <p><span><TiSocialInstagram/></span>{""}Instagram</p>
+                        <p><span><TiSocialLinkedin/></span>{""}LinkedIn</p>
+                        <p><span><TiSocialYoutube/></span>{""}Youtube</p>
+                    </div>
+                )}    
+
+                <BsThreeDots onClick = {()=>openReport()}
+                className = {Style.AuthorProfileCard_box_share_icon}
+                />                
+                {report && (
+                    <p className= {Style.AuthorProfileCard_box_share_report}>
+                        <span>
+                            <MdOutlineReportProblem/>
+                        </span>{""}{""}
+                        Report abouse
+                    </p>
+                )}                    
+            </div>  
         </div>
     </div>
   )
